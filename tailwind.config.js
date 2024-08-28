87a3abaa20ccae0454f5d6ae1ea89057788321cf
unknown <vankur017@gmail.com>
2024-08-28 21:54:07 +0530

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./src/Components/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
